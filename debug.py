a699008d8ab56fd475dc1540f4c75e22499348fd
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-13 21:50:04 -0500

    Added chile report to debug.

--- debug.py
+++ debug.py
@@ -1,9 +1,11 @@
 import valency_anndata as val
 
-adata = val.datasets.polis("https://pol.is/report/r2dfw8eambusb8buvecjt")
+# adata = val.datasets.polis("https://pol.is/report/r2dfw8eambusb8buvecjt") # small
+adata = val.datasets.polis("https://pol.is/report/r29kkytnipymd3exbynkd") # Chile
 # adata = val.datasets.polis("https://pol.is/3ntrtcehas")
 
 print(adata)
-print(adata.X.shape)
+if adata.X is not None:
+    print(adata.X.shape)
 print(adata.X)
 print(adata.uns["schema"])
