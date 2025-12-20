b232daf293648a4eb3d6b50715acae0099daceda
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-20 15:04:05 -0500

    Stop adding prefix to re-exported docstrings. Too confusing.

--- src/valency_anndata/tools/__init__.py
+++ src/valency_anndata/tools/__init__.py
@@ -4,12 +4,7 @@ from scanpy.tools._umap import umap
 from scanpy.tools._leiden import leiden
 from ._kmeans import kmeans
 from ._polis import recipe_polis
-from ..utils import _reexport_with_doc
 
-pca  = _reexport_with_doc(pca)
-tsne = _reexport_with_doc(tsne)
-umap = _reexport_with_doc(umap)
-leiden = _reexport_with_doc(leiden)
 
 __all__ = [
     "kmeans",
--- src/valency_anndata/viz/__init__.py
+++ src/valency_anndata/viz/__init__.py
@@ -2,14 +2,9 @@ from scanpy.plotting._tools.scatterplots import (
     pca,
     umap,
 )
-from ..utils import _reexport_with_doc
-
 from ._langevitour import langevitour
 from .schematic_diagram import schematic_diagram
 
-
-pca = _reexport_with_doc(pca)
-umap = _reexport_with_doc(umap)
 
 __all__ = [
     "langevitour",
